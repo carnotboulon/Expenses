2056cb213c5cc0216fb02fb115e9939715417dd7
carnotboulon <arnaudboland@gmail.com>
2016-05-25 23:25:29 +0200

    With popup delete confirmation

---
+++ resources/JS/summaryExpenses.js
@@ -0,0 +1,23 @@
+$(function() {
+
+     $('table tr').dblclick(function(event){
+            var that = $(this);
+            console.log($(this).attr("id"));
+            //Updating popup text and link.
+            $( "#popupdialog #delExpense").html($(this).children("td.object").html()+ "?");
+             
+            $( "#deleteBtn" ).unbind().click(
+                function ()
+                {
+                    console.log("Removing:" + that.attr("id"));
+                    that.remove();                               //removes it from the html page.
+                    $.ajax("/remove?exp="+that.attr("id"));      //removes it from DB.
+                }
+            );         
+            $( "#popupDialog" ).popup( "open");
+        });
+  
+  
+});//end of master function.
+
+
