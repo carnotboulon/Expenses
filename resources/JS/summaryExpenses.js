0cd5a79dfc2789940c48324f2945d5fecd398244
carnotboulon <arnaudboland@gmail.com>
2016-05-25 23:26:49 +0200

    with some more comments

--- resources/JS/summaryExpenses.js
+++ resources/JS/summaryExpenses.js
@@ -1,23 +1,21 @@
 $(function() {
-
-     $('table tr').dblclick(function(event){
-            var that = $(this);
-            console.log($(this).attr("id"));
-            //Updating popup text and link.
-            $( "#popupdialog #delExpense").html($(this).children("td.object").html()+ "?");
-             
-            $( "#deleteBtn" ).unbind().click(
-                function ()
-                {
-                    console.log("Removing:" + that.attr("id"));
-                    that.remove();                               //removes it from the html page.
-                    $.ajax("/remove?exp="+that.attr("id"));      //removes it from DB.
-                }
-            );         
-            $( "#popupDialog" ).popup( "open");
+    //Attach dble click handler on table elements.
+    $('table tr').dblclick(function(event){
+        var that = $(this);
+        console.log($(this).attr("id"));
+        //Updating popup text and link.
+        $( "#popupdialog #delExpense").html($(this).children("td.object").html()+ "?");
+        $( "#deleteBtn" ).unbind().click(
+            function ()
+            {
+                console.log("Removing:" + that.attr("id"));
+                that.remove();                               //removes it from the html page.
+                $.ajax("/remove?exp="+that.attr("id"));      //removes it from DB.
+            }
+        );         
+        $( "#popupDialog" ).popup( "open");
         });
-  
-  
+    
 });//end of master function.
 
 
