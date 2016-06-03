618ddf2390e5507e35cac76faf425fd6c38dafa4
carnotboulon <arnaudboland@gmail.com>
2016-06-03 19:08:41 +0200

    with Double Tap

--- resources/JS/summaryExpenses.js
+++ resources/JS/summaryExpenses.js
@@ -15,6 +15,24 @@ $(function() {
         );         
         $( "#popupDialog" ).popup( "open");
         });
+        
+        
+    $('table tr').on('tap', function() {
+        console.log($(this).attr("id"));
+        var that = $(this);
+        //Updating popup text and link.
+        $( "#popupdialog #delExpense").html($(this).children("td.object").html()+ "?");
+        $( "#deleteBtn" ).unbind().click(
+            function ()
+            {
+                console.log("Removing:" + that.attr("id"));
+                that.remove();                               //removes it from the html page.
+                $.ajax("/remove?exp="+that.attr("id"));      //removes it from DB.
+            }
+        );         
+        $( "#popupDialog" ).popup( "open");
+        });
+  
     
 });//end of master function.
 
